a1fdeb1bbe5875d385187481ef249b4fc312f61c
zhouliang6 <18510971680@163.com>
2017-07-25 11:20:43 +0800

    模板模式的完成

--- app/src/main/java/com/ai/kara/demo/Template/AbstractImageLoader.java
+++ app/src/main/java/com/ai/kara/demo/Template/AbstractImageLoader.java
@@ -3,6 +3,7 @@ package com.ai.kara.demo.Template;
 import android.content.Context;
 import android.widget.ImageView;
 
+import com.ai.kara.demo.R;
 import com.bumptech.glide.Glide;
 
 /**
@@ -13,7 +14,7 @@ public abstract class AbstractImageLoader {
 
     public final void downLoadImage(Context context, ImageView imageView, String imageUrl, int width, int height) {
         String downLoadImageUrl = getUrl(imageUrl, width, height);
-        Glide.with(context).load(downLoadImageUrl).into(imageView);
+        Glide.with(context).load(downLoadImageUrl).animate(R.anim.item_alpha_in).into(imageView);
     }
     protected abstract String getUrl(String imageUrl, int width, int height);
 
